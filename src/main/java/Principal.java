fea1097c862a08d1db77589bc8030487aa78c927
ivaxs <ivaxs@10.146.193.95>
2025-04-14 11:36:53 -0300

    inicio do projeto

---
+++ src/main/java/Principal.java
@@ -0,0 +1,17 @@
+public class Principal {
+
+       public static void main(String[] args) {
+
+    
+    Triangulo triangulo = new Triangulo();
+        triangulo.leitura();
+            System.out.println("area do triângulo:" + triangulo.getArea());
+            
+
+    Circulo circulo = new Circulo("Branco",3);
+        System.out.println(circulo.paraString());
+            System.out.println("area do círculo:" + circulo.getArea());
+                
+       }
+       
+}
